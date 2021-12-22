ebc51608ea51060cd15e697965f681e97e50cd2d
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2021-12-22 11:21:05 +0000

    upladed from replit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("PORT STARTERD UP! READY TO RECIEVE HTTP TRAFFIC!")
+
+
+function myalert() {
+alert("This website is currently in development. made with ❤ by alexwegrzyn183@gmail.com.");
+}
+
+//if a mobile user is on the desktop site it will redirect them to the mobile website.
+if (screen.width <= 699) { 
+document.location = "https://mobile-website-example.penguinbotcoder.repl.co";
+}
+
+
